8324fec82a7c0b95b6212137e2d771606440263e
vennavijay88 <58090356+vennavijay88@users.noreply.github.com>
2024-06-27 14:57:37 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -2,5 +2,11 @@
 
 int StringCalculator::add(string input)
 {
+   if (numbers.empty()) {
+        return 0;
+   }
+    if (numbers.find(',') == std::string::npos) {
+        return std::stoi(numbers);
+    }
         return 0;
 }
