567797ae749dde76d29f709dddf61c1fb10f9ad8
vennavijay88 <58090356+vennavijay88@users.noreply.github.com>
2024-06-27 15:27:08 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,9 +1,12 @@
 #include<iostream>
 #include<string>
+#include <vector>
 
 using namespace std;
 class StringCalculator
 {
    public:
    int add(string input);
+   private:
+   std::vector<int> splitNumbers(const std::string& numbers);
 };
