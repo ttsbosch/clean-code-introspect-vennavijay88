288ea452d24444dcd77ae0ee6bc82c5809984c23
vennavijay88 <58090356+vennavijay88@users.noreply.github.com>
2024-06-27 14:51:19 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -18,7 +18,7 @@ TEST(StringCalculatorAddTests, ExpectZeroForSingleZero) {
 
     ASSERT_EQ(result, expectedresult);
 }
-/*
+
 TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
     int expectedresult = 3;
     std::string input = "1,2";
@@ -27,7 +27,7 @@ TEST(StringCalculatorAddTests, ExpectSumForTwoNumbers) {
 
     ASSERT_EQ(result, expectedresult);
 }
-
+/*
 TEST(StringCalculatorAddTests, ExpectExceptionForNegativeNumbers) {
     ASSERT_THROW({
         std::string input = "-1,2";
