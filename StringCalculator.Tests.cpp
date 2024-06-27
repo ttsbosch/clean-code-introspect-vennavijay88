b529aa67ec04b1201ee12898a7823f5c8d5e1f55
vennavijay88 <58090356+vennavijay88@users.noreply.github.com>
2024-06-27 15:48:12 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -44,7 +44,7 @@ TEST(StringCalculatorAddTests, ExpectSumWithNewlineDelimiter) {
 
     ASSERT_EQ(result, expectedresult);
 }
-/*
+
 TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
     int expectedresult = 1;
     std::string input = "1,1001";
@@ -53,7 +53,7 @@ TEST(StringCalculatorAddTests, IgnoreNumbersGreaterThan1000) {
 
     ASSERT_EQ(result, expectedresult);
 }
-
+/*
 TEST(StringCalculatorAddTests, ExpectSumWithCustomDelimiter) {
     int expectedresult = 3;
     std::string input = "//;\n1;2";
